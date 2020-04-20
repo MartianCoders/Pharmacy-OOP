ded8f50c69d6f299f0d093c094df7c672d77f0a8
Teodor Ganga <info.teodor@gmail.com>
2020-04-20 18:27:44 +0300

    main

--- app.cpp
+++ app.cpp
@@ -2,8 +2,19 @@
 #include <crtdbg.h>
 
 #include "tests.h"
+#include "UI.h"
+#include "ControllerDrug.h"
+#include "RepositoryDrug.h"
+#include "RepositoryEmployee.h"
+#include "ControllerEmployee.h"
 
 int main() {
 	test_all();
+	RepositoryDrug storageDrugs("drugs.txt");
+	RepositoryEmployee storageEmployee("employee.txt");
+	ControllerDrug controllerDrug(storageDrugs);
+	ControllerEmployee controllerEmployee(storageEmployee);
+	UI ui(controllerDrug, controllerEmployee);
+	ui.menu();
 	_CrtDumpMemoryLeaks();
 }
